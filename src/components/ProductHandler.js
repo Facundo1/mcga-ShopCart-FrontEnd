e5d7168187fad42ee25a8f9515d457f67457f3fb
Facundo Loberse <facundosa123@gmail.com>
2020-03-08 15:53:58 -0300

    added delete and update buttons to the component

--- src/components/ProductHandler.js
+++ src/components/ProductHandler.js
@@ -7,3 +7,40 @@ import {
   setProductOnForm
 } from '../redux/actions/productActions'
 import { addToCart } from '../redux/actions/cartActions'
+
+class ProductsHandler extends Component {
+  componentDidMount() {
+    this.props.fetchProducts()
+  }
+
+  render() {
+    const productItems = this.props.products.map(product => (
+      <div className='col-md-4' key={product.id}>
+        <div className='thumbnail text-center'>
+          <a
+            href={`#${product._id}`}
+            onClick={() => this.props.addToCart(this.props.cartItems, product)}
+          >
+            <img src={product.photo} alt='photo' />
+            <p>{product.title}</p>
+          </a>
+          <b>{util.formatCurrency(product.price)}</b>
+          <button
+            className='btn btn-danger btn-xs'
+            onClick={() => this.props.deleteProduct(product._id)}
+          >
+            Delete Product
+          </button>
+          <button
+            className='btn-update'
+            onClick={() => this.props.setProductOnForm(product._id)}
+          >
+            Update Product
+          </button>
+        </div>
+      </div>
+    ))
+
+    return <div className='row'>{productItems}</div>
+  }
+}
